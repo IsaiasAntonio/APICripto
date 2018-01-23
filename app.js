89847ca0c01dd0329b05f958d96803b0e650130b
Terriquez <pedroterriquez@hotmail.com>
2018-01-23 11:50:39 -0600

    Configuring app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 jQuery('document').ready(function(){
     var miner = 'd7049af37A18BEDC9A85FE7b378f6085F17050C6';
     $.get("https://api.ethermine.org/miner/:miner/workers", function(data){
@@ -5,3 +6,12 @@ jQuery('document').ready(function(){
         alert("");
     });
 });
+
+function getPayout(miner)
+{
+    $.get( "https://api.ethermine.org/miner/:miner/payouts", function( data ) {
+        $( "#js-payout" ).html( data );
+        alert( "Load was performed." );
+      });
+}
+
