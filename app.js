827fd85f8c032692abc90df056c88ae793ef6fc2
IAOR <isaororio@gmail.com>
2018-01-23 11:52:57 -0600

    local variable

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 jQuery('document').ready(function(){
-    var miner = $('worker')
+    var miner = 'd7049af37A18BEDC9A85FE7b378f6085F17050C6';
+    $.get("https://api.ethermine.org/miner/:miner/workers", function(data){
+        $(".result").html(data);
+        alert("");
+    });
 });
