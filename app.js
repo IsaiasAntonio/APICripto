d0e439258bca5886652e5b8b68a0b8109c506ea6
Terriquez <pedroterriquez@hotmail.com>
2018-01-23 12:44:13 -0600

    Getting the info from CriptoApi

--- app.js
+++ app.js
@@ -5,12 +5,23 @@ jQuery('document').ready(function(){
         $(".result").html(data);
         alert("");
     });
+
+    getPayout(miner);
 });
 
 function getPayout(miner)
 {
-    $.get( "https://api.ethermine.org/miner/:miner/payouts", function( data ) {
-        $( "#js-payout" ).html( data );
+    $.get( "https://api.ethermine.org/miner/d7049af37A18BEDC9A85FE7b378f6085F17050C6/payouts", function( data ) {
+        
+        for(var i in data.data) {
+            $( "#content" ).append("<tr>")
+            $( "#content" ).append("<th>" + data.data[i].paidOn+ "</th>")
+            $( "#content" ).append("<th>" + data.data[i].start+ "</th>")
+            $( "#content" ).append("<th>" + data.data[i].end+ "</th>")
+            $( "#content" ).append("<th>" + data.data[i].amount+ "</th>")
+            $( "#content" ).append("<th>" + data.data[i].txHash+ "</th>")
+            $( "#content" ).append("</tr>")
+        }
         alert( "Load was performed." );
       });
 }
